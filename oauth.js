3c28dda45ce96ee4bb4390a0bf5b55ea32b3e3d7
Nitya Dhanushkodi <nitya.dhanushkodi@students.olin.edu>
2015-03-13 07:51:41 -0400

    oops

--- oauth.js
+++ oauth.js
@@ -3,8 +3,8 @@ facebook: {
  // clientID: process.env.FacebookID,
  // clientSecret: process.env.FacebookSecret,
  //callbackURL: process.env.callback || 
- clientID: "456765331140181",
- clientSecret: "c4941ebc78aeda1ff9d5c2863fb4b1b0",
+ clientID: "",
+ clientSecret: "",
  callbackURL: 'http://localhost:3000/auth/facebook/callback'
 },
 twitter: {
